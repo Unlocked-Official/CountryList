dd49df813ff27710aa07e02903d911e60ecd98a7
Tuan Nguyen <tuannguyenanh177@gmail.com>
2018-08-29 12:33:55 +0700

    Update CountryCell.swift

--- CountryList/CountryCell.swift
+++ CountryList/CountryCell.swift
@@ -12,12 +12,13 @@ class CountryCell: UITableViewCell {
     
     var nameLabel: UILabel?
     var extensionLabel: UILabel?
-    
+    var flagImage: UILabel?
     var country: Country? {
         didSet {
             if let name = country!.name {
                 nameLabel?.text = "\(name)"
                 extensionLabel?.text = "+\(country!.phoneExtension)"
+                flagImage?.text =  (country?.flag)!
             }
         }
     }
@@ -39,6 +40,15 @@ class CountryCell: UITableViewCell {
     
     func setup() {
         
+        flagImage = UILabel()
+        flagImage?.translatesAutoresizingMaskIntoConstraints = false
+        self.addSubview(flagImage!)
+        
+        //flagImage?.leftAnchor.constraint(equalTo: self.leftAnchor, constant: 15).isActive = true
+        //flagImage?.frame = CGRect(x: 10, y: 10, width: 30, height: 20)
+        flagImage?.centerYAnchor.constraint(equalTo: self.centerYAnchor).isActive = true
+        flagImage?.leftAnchor.constraint(equalTo: self.leftAnchor, constant: 15).isActive = true
+        
         nameLabel = UILabel()
         nameLabel?.textColor = UIColor.black
         nameLabel?.font = UIFont.systemFont(ofSize: 20)
@@ -46,14 +56,7 @@ class CountryCell: UITableViewCell {
         self.addSubview(nameLabel!)
         
         nameLabel?.centerYAnchor.constraint(equalTo: self.centerYAnchor).isActive = true
-        nameLabel?.leftAnchor.constraint(equalTo: self.leftAnchor, constant: 15).isActive = true
+        nameLabel?.leftAnchor.constraint(equalTo: self.leftAnchor, constant: 50).isActive = true
         
-        extensionLabel = UILabel()
-        extensionLabel?.textColor = UIColor.gray
-        extensionLabel?.font = UIFont.systemFont(ofSize: 18)
-        extensionLabel?.translatesAutoresizingMaskIntoConstraints = false
-        addSubview(extensionLabel!)
-        extensionLabel?.centerYAnchor.constraint(equalTo: self.centerYAnchor).isActive = true
-        extensionLabel?.rightAnchor.constraint(equalTo: self.rightAnchor, constant: -15).isActive = true
     }
 }
